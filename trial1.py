f34e90e4a970540e8ef76a35cd86dc8d0cb7493f
Pakshat1 <patelak1201@gmail.com>
2025-06-18 13:00:22 +0530

    updated file

---
+++ trial1.py
@@ -0,0 +1,30 @@
+#  input 2 numbres and print sum
+
+first=int(input("enter first number: "))
+second=int(input("enter second number: "))
+
+print("sum= " ,(first + second))
+
+
+# input side of a squre and print area
+
+side=float(input("enter the value of side:"))
+
+print("area=",(side*side))
+
+# # input 2 floating number and print the avrage
+
+a=float(input("enter a:"))
+b=float(input("enter b:"))
+
+print("avg=",(a+b)/2)
+
+# input 2 int numbers,a and b print if a is grater or equal to b , if not print false
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+
+print(a>=b)
+
+
+
