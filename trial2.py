ce83d928eb21c943af157ab6fbb987d418cb6874
Pakshat1 <patelak1201@gmail.com>
2025-06-18 12:59:33 +0530

    updated file

---
+++ trial2.py
@@ -0,0 +1,15 @@
+# # Define a function named string_length that takes one argument, str1.
+def stringth_length(str):
+    count=0
+    for char in str:
+        count+= 1
+    return count
+print(stringth_length("hello my name is Akshat"))
+
+
+# # WAP to input users first name and print the length
+
+name=input("enrtr your name: ")
+print("length of your name is",len(name))
+
+# WAP to find the occurance of "$" in a string
