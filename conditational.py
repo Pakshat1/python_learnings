f7bc754837abfa552b4c3f7b0942bc5b52409255
Pakshat1 <patelak1201@gmail.com>
2025-06-20 10:23:34 +0530

    updated file

--- conditational.py
+++ conditational.py
@@ -16,3 +16,25 @@ else:
   print("You cannot drive")
   print("No")
   print("Yay!")
+
+
+applePrice = 10
+budget = 200
+if (budget - applePrice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
+
+#elseif 
+ 
+num = int(input("Enter the value of num: "))
+if (num < 0):
+  print("Number is negative.")
+elif (num == 0):
+  print("Number is Zero.")
+elif (num == 999):
+  print("Number is Special.")
+else:
+  print("Number is positive.")
+
+print("I am happy now")
