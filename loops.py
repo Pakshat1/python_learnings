9e18c31b28b634151cc5e458a23b675e204cd08d
Pakshat1 <patelak1201@gmail.com>
2025-06-20 15:19:15 +0530

    updated file

---
+++ loops.py
@@ -0,0 +1,35 @@
+# for loop
+
+color="red blue green yellow"
+for color in color:
+    print(color)
+   
+for i in color:
+    print(i)
+
+
+# practice 1
+
+colors="red blue green yellow"
+for color in colors:
+    print(color )
+
+
+#   range()
+
+for k in range(0,10):
+    print(k+2)
+
+# calculate factorial
+n=(int(input("enter the number")))
+factorial=1
+if n<0:
+    print("factorial does not exixt for negative number")
+elif n==0:
+    print("factorial of 0 is 1")
+else:
+    for i in range(1, n + 1):
+        factorial *= i
+    print(f"The factorial of {n} is {factorial}.")
+
+        
