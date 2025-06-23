6c570e6e1c99efd381ab2601e4b595e482e14c82
Pakshat1 <patelak1201@gmail.com>
2025-06-23 14:21:18 +0530

    updated file

---
+++ whileloop.py
@@ -0,0 +1,36 @@
+# while loop
+# count number 1 to 10
+
+num = 1
+while num <= 10:
+    print(num)
+    num += 1 
+
+#  Sum of numbers from 1 to 100
+num = 1
+total = 0
+while num <= 100:
+    total += num
+    num += 1
+print("Sum is:", total)
+
+# count 5 in desending order using while loop
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+
+# Else with While Loop
+# count 5 in desending order
+x = 5
+while (x > 0):
+    print(x)
+    x = x - 1
+else:
+    print('counter is 0')
+
+
+
+
+
+
