2985e2d8f6cf9c59bd40209dcedd1181c3d337c8
Pakshat1 <patelak1201@gmail.com>
2025-06-26 16:01:39 +0530

    updated file

---
+++ docstring.py
@@ -0,0 +1,25 @@
+# doc strings are used to describe the purpose of a function, class, or module.
+# They are written in triple quotes and can span multiple lines.    
+
+def add(a, b):
+    """
+    Adds two numbers together.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The sum of the two numbers.
+    """
+    return a + b
+a=1
+b=2
+result = add(a, b)
+print(f"The result of adding {a} and {b} is {result}.")
+# output: The result of adding 1 and 2 is 3. 
+
+
+
+ 
+
