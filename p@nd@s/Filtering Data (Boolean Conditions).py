73061c4f24109369c2248dca84f2519e99c13929
Pakshat1 <patelak1201@gmail.com>
2025-07-08 12:50:22 +0530

    updated file

--- p@nd@s/Filtering Data (Boolean Conditions).py
+++ p@nd@s/Filtering Data (Boolean Conditions).py
@@ -33,3 +33,47 @@ print(df[df['Marks'].between(85, 90)])
 # 6. Using query()
 print("\n6. Using query() method (Marks > 85 and Age < 24):")
 print(df.query("Marks > 85 and Age < 24"))
+
+
+# # output 
+# Original DataFrame:
+#       Name  Marks  Age
+# 0    Alice     88   23
+# 1      Bob     92   22
+# 2  Charlie     85   24
+# 3    David     90   23
+
+# 1. Marks > 85:
+#     Name  Marks  Age
+# 0  Alice     88   23
+# 1    Bob     92   22
+# 3  David     90   23
+
+# 2. Marks > 85 AND Age < 24:
+#     Name  Marks  Age
+# 0  Alice     88   23
+# 1    Bob     92   22
+# 3  David     90   23
+
+# 3. Marks > 90 OR Age == 23:
+#     Name  Marks  Age
+# 0  Alice     88   23
+# 1    Bob     92   22
+# 3  David     90   23
+
+# 4. Name is Alice or David:
+#     Name  Marks  Age
+# 0  Alice     88   23
+# 3  David     90   23
+
+# 5. Marks between 85 and 90:
+#       Name  Marks  Age
+# 0    Alice     88   23
+# 2  Charlie     85   24
+# 3    David     90   23
+
+# 6. Using query() method (Marks > 85 and Age < 24):
+#     Name  Marks  Age
+# 0  Alice     88   23
+# 1    Bob     92   22
+# 3  David     90   23
